c1ae0fa26e2f799307a5f9e4d357929134b3ff3a
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-09-27 21:03:29 -0400

    form input state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Form } from "react-bootstrap";
 
 const AddForm = () => {
+
+  const [input, setInput] = useState({
+        name: "",
+        age: "",
+        note: "",
+        img: ""
+    })
     return (
       <div className='AddToList'>
         <Form>
-          <Form.Group className="mb-3">
+          <Form.Group className="mb-2">
             <Form.Control type="text" placeholder="Enter movie name" 
                 className="AddToList-input"
                 name="name"
                />
           </Form.Group>
-           <Form.Group className="mb-3">
+           <Form.Group className="mb-2">
             <Form.Control type="text" placeholder="Enter movie year" 
                 className="AddToList-input"
                 name="year"
                />
         </Form.Group>
-           <Form.Group className="mb-3">
+           <Form.Group className="mb-2">
           <Form.Control type="text" placeholder="Enter image url" 
                 className="AddToList-input"
                 name="url"
                />
           </Form.Group>
-           <Form.Group className="mb-3">
+           <Form.Group className="mb-2">
         <Form.Control type="text" placeholder="Enter movie description" 
                 className="AddToList-input"
                 name="desc"
